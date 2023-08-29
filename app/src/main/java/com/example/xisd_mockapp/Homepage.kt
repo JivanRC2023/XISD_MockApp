6bf60231bad2857476f6d5cec0486cb0d21c2d5c
jivan <jivanmoodley@gmail.com>
2023-08-29 21:45:22 +0200

    done

--- app/src/main/java/com/example/xisd_mockapp/Homepage.kt
+++ app/src/main/java/com/example/xisd_mockapp/Homepage.kt
@@ -1,13 +1,56 @@
 package com.example.xisd_mockapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
 
-        
+        val buttonA: Button = findViewById(R.id.Account_btn)
+        buttonA.setOnClickListener {
+            val intent = Intent(this, account::class.java)
+            startActivity(intent)
+        }
+
+        val buttonNews: Button = findViewById(R.id.News_btn)
+        buttonNews.setOnClickListener {
+            val intent = Intent(this, news::class.java)
+            startActivity(intent)
+        }
+
+        val buttonWeb: Button = findViewById(R.id.Website_btn)
+        buttonWeb.setOnClickListener {
+            val intent = Intent(this, news::class.java)
+            startActivity(intent)
+        }
+
+        val buttonCont: Button = findViewById(R.id.Contact_btn)
+        buttonCont.setOnClickListener {
+            val intent = Intent(this, contact::class.java)
+            startActivity(intent)
+        }
+
+        val buttonServ: Button = findViewById(R.id.Services_btn)
+        buttonServ.setOnClickListener {
+            val intent = Intent(this, services::class.java)
+            startActivity(intent)
+        }
+
+        val buttonDon: Button = findViewById(R.id.Donate_btn)
+        buttonDon.setOnClickListener {
+            val intent = Intent(this, donate::class.java)
+            startActivity(intent)
+        }
+
+        val buttonReq: Button = findViewById(R.id.Request_btn)
+        buttonReq.setOnClickListener {
+            val intent = Intent(this, request::class.java)
+            startActivity(intent)
+        }
+
     }
 }
