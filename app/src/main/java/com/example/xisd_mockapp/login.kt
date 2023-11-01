331406f127bcf6912ddba00fa251d741fbdf01e4
jivan <jivanmoodley@gmail.com>
2023-11-01 09:19:56 +0200

    done

--- app/src/main/java/com/example/xisd_mockapp/login.kt
+++ app/src/main/java/com/example/xisd_mockapp/login.kt
@@ -1,18 +1,43 @@
 package com.example.xisd_mockapp
 
+import android.accounts.Account
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.example.xisd_mockapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class login : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val buttonLI: Button = findViewById(R.id.loginpage_btn)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val buttonLI: Button = findViewById(R.id.login_btn)
         buttonLI.setOnClickListener {
-            val intent = Intent(this, homepage::class.java)
+            val email = binding.LoginEmail.text.toString()
+            val password = binding.LoginPassword.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val intent = Intent(this, Account::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            }
+
+            val intent = Intent(this, Homepage::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/xisd_mockapp/signup.kt
+++ app/src/main/java/com/example/xisd_mockapp/signup.kt
@@ -1,19 +1,57 @@
 package com.example.xisd_mockapp
 
+import android.accounts.Account
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.xisd_mockapp.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 
 
 class signup : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+
+        var username = "user";
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         val buttonSU: Button = findViewById(R.id.thanksReturn_btn)
         buttonSU.setOnClickListener {
-            val intent = Intent(this, homepage::class.java)
+
+            val email = binding.SignupEmail.text.toString()
+            val password = binding.SignupPassword.text.toString()
+            val name = binding.SignupName.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && name.isNotEmpty()) {
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+
+                            val profileUpdates = UserProfileChangeRequest.Builder()
+                                .setDisplayName(username)
+                                .build()
+
+                            val intent = Intent(this, Account::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            }
+
+            //new page
+            val intent = Intent(this, Homepage::class.java)
             startActivity(intent)
         }
 
